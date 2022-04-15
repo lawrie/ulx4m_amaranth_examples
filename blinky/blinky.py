056a7089ed074ea39b5454d4ab5073d2d438cbf5
lawrie <lawrie.griffiths@ntlword.com>
2022-04-15 13:52:24 +0100

    Ulx4m Amaranth blinky

---
+++ blinky/blinky.py
@@ -0,0 +1,30 @@
+import argparse
+
+from amaranth import *
+from ulx4m import *
+
+class Blinky(Elaboratable):
+    def elaborate(self, platform):
+        led   = platform.request("led", 0)
+        timer = Signal(24)
+
+        m = Module()
+        m.d.sync += timer.eq(timer + 1)
+        m.d.comb += led.o.eq(timer[-1])
+        return m
+
+
+if __name__ == "__main__":
+    variants = {
+        '12F': ULX4M_12F_Platform,
+        '45F': ULX4M_45F_Platform,
+        '85F': ULX4M_85F_Platform
+    }
+
+    # Figure out which FPGA variant we want to target...
+    parser = argparse.ArgumentParser()
+    parser.add_argument('variant', choices=variants.keys())
+    args = parser.parse_args()
+
+    platform = variants[args.variant]()
+    platform.build(Blinky(), do_program=True)
